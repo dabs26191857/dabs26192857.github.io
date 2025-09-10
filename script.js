db840837030c5651015e6d81b211c7364107a852
dabs26191857 <dabs26191857@gmail.com>
2025-09-10 08:00:40 +0000

    更新網站內容

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-//繪製漸變
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
 
-// Create gradient
-var grd = ctx.createRadialGradient(75, 50, 5, 90, 60, 100);
-grd.addColorStop(0, "red");
-grd.addColorStop(1, "white");
-
-// Fill with gradient
-ctx.fillStyle = grd;
-ctx.fillRect(10, 10, 150, 80);
 // 取得所有 dropdown 按鈕
 let dropdowns = document.querySelectorAll(".dropdown-btn");
 
